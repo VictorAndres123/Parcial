9638ae3b1a1c06381e3b1f4761c408ab2e70082f
VictorAndres123 <132307617+VictorAndres123@users.noreply.github.com>
2024-05-20 20:31:09 -0500

    Add files via upload

---
+++ frond/src/views/HomePage.vue
@@ -0,0 +1,58 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Home</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div id="container">
+        <strong>Welcome to Home Page</strong>
+        <p>Click below links to navigate:</p>
+        <ion-button router-link="/categories">Categories</ion-button>
+        <ion-button router-link="/cars">Cars</ion-button>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/vue';
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container ion-button {
+  margin-top: 20px;
+}
+</style>
